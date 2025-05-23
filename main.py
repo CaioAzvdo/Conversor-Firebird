8e4bce5c830790f4698515d3a19dae1470d1e976
CaioAzvdo <caioazevedodourado8@gmail.com>
2025-05-22 22:22:32 -0300

    feat:add new query sql

--- main.py
+++ main.py
@@ -1,17 +1,46 @@
 import pandas as pd
 import fdb
 
-# Conectar ao banco Firebird
+
 con = fdb.connect(
-    dsn=r'localhost:C:\Users\kaioc\Downloads\DADOS - Copia\DADOS\INTEGRADO.GDB',  # Exemplo: 'localhost:C:/meubanco/dados.fdb'
+    dsn=r'localhost:C:\Users\kaioc\Downloads\DADOS\DADOS\INTEGRADO.GDB',
     user='SYSDBA',
     password='masterkey',
     charset='UTF8'
-    # fb_library_name=r'C:\Program Files\Firebird\Firebird_2_5\bin\fbclient.dll'
-
 )
 
-query = "SELECT * FROM TABPRO"
+query = """
+SELECT
+    t.CODPRO AS "COD_PRODUTO",
+    '' AS COD_GRADE,
+    t.DESCPRO AS "DESC_PRODUTO",
+    t.CST AS "COD_IMPOSTO",
+    '' AS "DESC_IMPOSTO",
+    '' AS "REFERENCIA",
+    t.CODBARUN AS "COD_BARRA",
+    '' AS "COD_BALANCA",
+    t.PESOLIQUIDO AS "PESO_LIQUIDO",
+    t.PESOBRUTO  AS "PESO_BRUTO",
+    t.UNIDADE AS "UNIDADE",
+    (SELECT FIRST 1 tf.PRVAPRO 
+     FROM TABPROFIL tf 
+     WHERE tf.CODPRO = t.CODPRO 
+     ORDER BY tf.PRVAPRO DESC) AS "VR_PRAZO",
+    '' AS "VR_VISTA",
+    (SELECT FIRST 1 tf.PRCUPRO 
+     FROM TABPROFIL tf 
+     WHERE tf.CODPRO = t.CODPRO 
+     ORDER BY tf.PRVAPRO DESC) AS "VR_CUSTO",
+    '' AS "OBS",
+    t.CLASFISCAL AS "NCM",
+    '' AS "FLAG_ATIVO",
+    t.GRUPRO AS "COD_GRUPO",
+    tg.NOMGRU AS "DESC_GRUPO",
+    t.CEST AS "CEST"
+FROM TABPRO t
+LEFT JOIN TABGRU tg ON tg.CODGRU = t.GRUPRO;
+
+"""
 cur = con.cursor()
 cur.execute(query)
 columns = [desc[0] for desc in cur.description]
